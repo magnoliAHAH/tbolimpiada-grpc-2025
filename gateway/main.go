3011f544379e8e9e859e536af2971e6d07ddd7a4
magnoliAHAH <mixail.ermin33@gmail.com>
2025-03-18 15:03:49 +0300

    add meeting point calculate logic & saving file into /tmp

--- gateway/main.go
+++ gateway/main.go
@@ -1,32 +1,72 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 
-	pb "github.com/magnoliAHAH/protos-tbolimpiada/gen" // Должно совпадать с go_package в .proto
+	pb "github.com/magnoliAHAH/protos-tbolimpiada/gen"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
+// Подключаемся к gRPC серверу
+var client pb.ProcessingServiceClient
+
 func main() {
 	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
-
 	if err != nil {
-		log.Fatalf("Ошибка подключения: %v", err)
+		log.Fatalf("Ошибка подключения к gRPC серверу: %v", err)
 	}
 	defer conn.Close()
 
-	client := pb.NewProcessingServiceClient(conn)
+	client = pb.NewProcessingServiceClient(conn)
 
-	resp, err := client.ProcessFile(context.Background(), &pb.ProcessRequest{
-		Filename: "test.txt",
-		Content:  []byte("Hello, world!"),
-	})
-	if err != nil {
-		log.Fatalf("Ошибка при вызове ProcessFile: %v", err)
+	// HTTP обработчик для загрузки файлов
+	http.HandleFunc("/upload", uploadHandler)
+
+	log.Println("HTTP Gateway запущен на порту 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+// uploadHandler обрабатывает загрузку файлов
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Только POST-запросы", http.StatusMethodNotAllowed)
+		return
 	}
 
-	fmt.Printf("Ответ сервера: %s\n", resp.MeetingPoint)
+	// Читаем файл из запроса
+	file, header, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, "Ошибка при получении файла", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	// Читаем содержимое файла в буфер
+	var buf bytes.Buffer
+	_, err = io.Copy(&buf, file)
+	if err != nil {
+		http.Error(w, "Ошибка при чтении файла", http.StatusInternalServerError)
+		return
+	}
+
+	// Отправляем файл на gRPC-сервер
+	resp, err := client.ProcessFile(context.Background(), &pb.ProcessRequest{
+		Filename: header.Filename,
+		Content:  buf.Bytes(),
+	})
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Ошибка вызова gRPC: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	// Возвращаем результат клиенту
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Файл обработан, точка сбора: %s\n", resp.MeetingPoint)
 }
